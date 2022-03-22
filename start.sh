ace5ecd2ff430e116b724860778cb630fe274803
sysujerome <1570992296@qq.com>
2022-03-22 19:25:45 +0800

    debugging

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@
 # echo >> log
 # echo $current_date_time >> log
 
-# go build -o ./bin/server ./server/main.go
-# go build -o ./bin/client ./client/main.go
-# ps -ef | grep ./bin/server | grep -v grep | awk '{print $2}' | xargs kill -9
-# ps -ef | grep ./bin/client | grep -v grep | awk '{print $2}' | xargs kill -9
-# # ./bin/server --shard_idx=0 &>> log &
-# # ./bin/server --shard_idx=1 &>> log &
-# # ./bin/server --shard_idx=2 &>> log &
-# # ./bin/server --shard_idx=3 &>> log &
-# # ./bin/server --shard_idx=4 &>> log &
-# # ./bin/server --shard_idx=5 &>> log &
-# # ./bin/server --shard_idx=6 &>> log &
-# # ./bin/server --shard_idx=7 &>> log &
+go build -o ./bin/server ./server/main.go
+go build -o ./bin/client ./client/main.go
+ps -ef | grep ./bin/server | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep ./bin/client | grep -v grep | awk '{print $2}' | xargs kill -9
+./bin/server --shard_idx=0 &>> log &
+./bin/server --shard_idx=1 &>> log &
+./bin/server --shard_idx=2 &>> log &
+./bin/server --shard_idx=3 &>> log &
+./bin/server --shard_idx=4 &>> log &
+./bin/server --shard_idx=5 &>> log &
+./bin/server --shard_idx=6 &>> log &
+./bin/server --shard_idx=7 &>> log &
 
 # sleep 5s
 # count=$1
